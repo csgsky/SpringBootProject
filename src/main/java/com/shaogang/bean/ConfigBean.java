525e8dad5a1d95c13295cb2fd46c37fcc9435d18
csgsky <674143140@qq.com>
2017-11-30 12:42:20 +0800

    添加热部署

--- src/main/java/com/shaogang/bean/ConfigBean.java
+++ src/main/java/com/shaogang/bean/ConfigBean.java
@@ -1,6 +1,8 @@
 package com.shaogang.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+
 /**
  * Created by allen on 17/11/28.
  */
@@ -9,6 +11,7 @@ import lombok.Data;
 //@ConfigurationProperties(prefix = "com.chen")
 //@PropertySource("classpath:test.properties")
 @Data
+@AllArgsConstructor
 public class ConfigBean {
     private String name;
     private String want;
--- src/main/java/com/shaogang/controller/UserController.java
+++ src/main/java/com/shaogang/controller/UserController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-
     @RequestMapping("/getuser")
     public ConfigBean hexo(){
-        ConfigBean configBean = new ConfigBean();
-        configBean.setName("小明");
-        configBean.setWant("新年快乐！");
+        ConfigBean configBean = new ConfigBean("小明", "新年快乐");
+        return configBean;
+    }
+
+    @RequestMapping("/getAnotherUser")
+    public ConfigBean getAnotherUser(){
+        ConfigBean configBean = new ConfigBean("小明明", "圣诞节快乐ll111");
         return configBean;
     }
 }
