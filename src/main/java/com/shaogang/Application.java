b32575fd1a28a8e6ad5c2d80815c6cc6348894c1
csgsky <674143140@qq.com>
2018-01-22 10:24:59 +0800

    cg lib 开发

--- src/main/java/com/shaogang/Application.java
+++ src/main/java/com/shaogang/Application.java
@@ -2,8 +2,11 @@ package com.shaogang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+// 强制使用 cglib 代理
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(new Object[]{
